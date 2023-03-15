2a3a7b337d9820027dfee3feb2c9d3bead55e2ec
debabrata-97 <debabrataszr@gmail.com>
2023-03-15 23:11:43 +0530

    User need to Enter a valid Last Name

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -1,17 +1,25 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
         UserRegistration ug = new UserRegistration();
         ug.firstName();
-
+        ug.lastName();
     }
 
     public void firstName() {
-        Pattern pattern = Pattern.compile("[A-Z][a-z]{2,}");
-        Matcher matcher = pattern.matcher("Debabrata");
-        boolean value = matcher.matches();
-        System.out.println("Result : "+value);
+        Pattern pattern1 = Pattern.compile("[A-Z][a-z]{2,}");
+        Matcher matcher1 = pattern1.matcher("Debabrata");
+        boolean value1 = matcher1.matches();
+        System.out.println("Result for First Name : " + value1);
+    }
+
+    public void lastName() {
+        Pattern pattern2 = Pattern.compile("[A-z][A-za-z]{2,}");
+        Matcher matcher2 = pattern2.matcher("Sahoo");
+        boolean value2 = matcher2.matches();
+        System.out.println("Result for Last Name : " + value2);
     }
 }
